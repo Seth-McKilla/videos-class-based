fc56f4e4a7b7fd306b7468dae91bbb305cacb87a
Seth McCullough <sethmckilladev@gmail.com>
2020-12-06 12:09:23 -0500

    Add youtube video fetch API

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../api/youtube";
+import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
 class App extends React.Component {
-  state = { videos: [] };
+  state = { videos: [], selectedVideo: null };
+
+  componentDidMount() {
+    this.onTermSubmit("buildings");
+  }
 
   onTermSubmit = async (term) => {
     const response = await youtube.get("/search", {
@@ -12,14 +18,33 @@ class App extends React.Component {
       },
     });
 
-    this.setState({ videos: response.data.items });
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0],
+    });
+  };
+
+  onVideoSelect = (video) => {
+    this.setState({ selectedVideo: video });
   };
 
   render() {
     return (
       <div className="ui container">
-        <SearchBar onTermSubmit={this.onTermSubmit} />I have{" "}
-        {this.state.videos.length} videos.
+        <SearchBar onTermSubmit={this.onTermSubmit} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                onVideoSelect={this.onVideoSelect}
+                videos={this.state.videos}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
